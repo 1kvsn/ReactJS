5becffcd5959e99a6885fdfac2b198e8cc7c7cf5
Sasikant <nairs999@gmail.com>
2019-03-20 10:06:59 +0530

    "Add tictactoe"

--- react_practice/src/App.js
+++ react_practice/src/App.js
@@ -1,28 +1,56 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Card from './Card';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      num1: 1,
+      num2: 1,
+      response: ""
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Card />
+        <div>
+          <div id="problem">
+            {this.state.num1} + {this.state.num2}
+          </div>
+          <input onKeyPress={this.inputKeyPress} onChange={this.updateResponse} value={this.state.response}/>
+          <p>Response is: {this.state.response}</p>
+        </div>
       </div>
     );
+  }
+
+  updateResponse = (event) => {
+    this.setState({response: event.target.value});
+  }
+
+  inputKeyPress = (event) => {
+    if(event.key === "Enter") {
+      const answer = parseInt(this.state.response);
+      if(answer === this.state.num1 + this.state.num2) {
+        this.setState({
+          num1: Math.ceil(Math.random() * 10),
+          num2: Math.ceil(Math.random() * 10),
+          response: ""
+        })
+      }
+    }
   }
 }
 
 export default App;
+
+// response = "" means by default there is nothing in the input field. Response is going to be the part of the state which keeps track of the user's input.
+//Whatever is in the this.state.response is going to be rendered in the input field.
+//When you've written the this.state.response line, if you check the input field and try to type anything, you'll find that it doesn't allow/show anything we type.
+//Reason: We've set the initialstate of response as "" and this is what the React is rendering using the this.state.response. It keeps on showing empty string in the input field.
+//Now, as our input field is taking the inputs, let's handle an event so that when the user inputs something and presses Enter, we can grab the input and do something with it.
+//
